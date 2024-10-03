5ff8e9c75a423a7a038f24693227304034cbb0e0
Abdul Haseeb <106963981+abdullhasseeb@users.noreply.github.com>
2024-10-03 10:15:42 +0500

    Updated

--- lib/utils/constants/api_urls.dart
+++ lib/utils/constants/api_urls.dart
@@ -3,7 +3,7 @@
 class HkApiUrls{
 
   //static const String apiKey = 'sandbox_8abf65a9156289945ddff6233de906fe17d2bdf7'; // Photo Room Api
-  static const String apiKey = 'SuZi4WPK7jwMS3HiJpSJwMG4'; // Remove.bg Api
+  static const String apiKey = 'YOUR_API_KEY'; // Remove.bg Api
 
   static const String baseUrl = 'https://api.remove.bg/v1.0/';
 
